6466745b5bb873764c549582335cc6daa55fe52b
Tiago Campêlo <tscs2@cin.ufpe.br>
2024-02-05 18:54:02 -0300

    Adiciona json de fluxos do exercicio e altera websocket para comunicar-se com node-red

--- websockets/ws-server.js
+++ websockets/ws-server.js
@@ -1,5 +1,5 @@
 var WebSocketServer = require('ws').Server;
-wss = new WebSocketServer({ port: 8080, path: '/testing' });
+wss = new WebSocketServer({ port: 8080, path: '/temperature' });
 wss.on('connection', function (ws) {
     ws.on('message', function (message) {
         console.log('Msg received in server: %s ', message);
@@ -7,3 +7,15 @@ wss.on('connection', function (ws) {
     console.log('new connection');
     ws.send('Msg from server');
 });
+
+wssVoltage = new WebSocketServer({ port: 8081, path: '/voltage' });
+wssVoltage.on('connection', function (ws) {
+    ws.on('message', function (message) {
+        console.log('Msg received in server: %s ', message);
+    });
+    ws.send(randomInt(0, 100).toString());
+});
+
+function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
